45db05ddaad42092987e56a2de2ed338137e74a8
robby martin <rdmartin2@uncg.edu>
2025-07-10 09:18:42 -0400

    Updated with cuisine suggestions and different css styling.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,22 @@
 import RecipeCard from "../components/RecipeCard";
 import "../css/Home.css";
 import { useState, useEffect } from "react";
-import { getRandomRecipes } from "../services/api";
+import { getRandomRecipes, getBritishCuisine, getGreekCuisine, getKoreanCuisine } from "../services/api";
 
 function Home() {
 
     const [randomRecipes, setRandomRecipes] = useState([]);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
+
+    const [britishRecipes, setBritishRecipes] = useState([]);
+    const [errorBritishCuisine, setErrorBritishCuisine] = useState(null);
+
+    const [greekRecipes, setGreekRecipes] = useState([]);
+    const [errorGreekCuisine, setErrorGreekCuisine] = useState(null);
+
+    const [koreanRecipes, setKoreanRecipes] = useState([]);
+    const [errorKoreanCuisine, setErrorKoreanCuisine] = useState(null);
     
     useEffect(() => {
        const loadRandomRecipes = async() => {
@@ -24,19 +33,110 @@ function Home() {
        loadRandomRecipes();
     }, []);
 
+    useEffect(() => {
+        const loadBritishRecipes = async() => {
+            try {
+                const britishRecipesList = await getBritishCuisine();
+                setBritishRecipes(britishRecipesList);
+            } catch (err) {
+                console.log(err);
+                setErrorBritishCuisine("Failed to load British cuisine...");
+            } finally {
+                setLoading(false);
+            }
+        }
+        loadBritishRecipes();
+    }, []);
+
+    useEffect(() => {
+        const loadGreekRecipes = async() => {
+            try {
+                const greekRecipesList = await getGreekCuisine();
+                setGreekRecipes(greekRecipesList);
+            } catch (err) {
+                console.log(err);
+                setErrorGreekCuisine("Failed to load Greek cuisine...");
+            } finally {
+                setLoading(false);
+            }
+        }
+        loadGreekRecipes();
+    }, []);
+
+    useEffect(() => {
+        const loadKoreanRecipes = async() => {
+            try {
+                const koreanRecipesList = await getKoreanCuisine();
+                setKoreanRecipes(koreanRecipesList);
+            } catch (err) {
+                console.log(err);
+                setErrorKoreanCuisine("Failed to load Korean cuisine...");
+            } finally {
+                setLoading(false);
+            }
+        }
+        loadKoreanRecipes();
+    }, [])
+
     return (
         <div className="home">
-
+            <br></br>
             {error && <div className="error-message">{error}</div>}
-
             {loading ? (
                 <div className="loading">Loading...</div>
             ) : (
-                <div className="random-recipe-grid">
+                <>
+                <h4>Random Recipes to try:</h4>
+                <div className="recipe-grid">
                     {randomRecipes.map((recipe) => (
                         <RecipeCard recipe={recipe} key={recipe.id}/>
                     ))}
                 </div>
+                </>
+            )}
+
+        
+            {errorBritishCuisine && <div className="error-message">{errorBritishCuisine}</div>}
+            {loading ? (
+                <div className="loading">Loading...</div>
+            ) : (
+                <>
+                <h4>British cuisine:</h4>
+                <div className="recipe-grid">
+                    {britishRecipes.map((recipe) => (
+                        <RecipeCard recipe={recipe} key={recipe.id} />
+                    ))}
+                </div>
+                </>
+            )}
+
+            <br></br>
+            {errorGreekCuisine && <div className="error-message">{errorGreekCuisine}</div>}
+            {loading ? (
+                <div className="loading">Loading...</div>
+            ) : (
+                <>
+                <h4>Greek cuisine:</h4>
+                <div className="recipe-grid">
+                    {greekRecipes.map((recipe) => (
+                        <RecipeCard recipe={recipe} key={recipe.id} />
+                    ))}
+                </div>
+                </>
+            )}
+
+            {errorKoreanCuisine && <div className="error-message">{errorKoreanCuisine}</div>}
+            {loading ? (
+                <div className="loading">Loading...</div>
+            ) : (
+                <>
+                <h4>Korean cuisine:</h4>
+                <div className="recipe-grid">
+                    {koreanRecipes.map((recipe) => (
+                        <RecipeCard recipe={recipe} key={recipe.id} />
+                    ))}
+                </div>
+                </>
             )}
             
         </div>
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,25 @@ const api_key = import.meta.env.VITE_API_KEY;
 const base_url = import.meta.env.VITE_API_URL;
 
 export const getRandomRecipes = async () => {
-    const response = await fetch(`${base_url}/recipes/random?apiKey=${api_key}&number=9`);
+    const response = await fetch(`${base_url}/recipes/random?apiKey=${api_key}&number=5`);
     const data = await response.json();
     return data.recipes;
+};
+
+export const getBritishCuisine = async () => {
+    const response = await fetch(`${base_url}/recipes/complexSearch?apiKey=${api_key}&cuisine=british&number=5`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const getGreekCuisine = async () => {
+    const response = await fetch(`${base_url}/recipes/complexSearch?apiKey=${api_key}&cuisine=greek&number=5`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const getKoreanCuisine = async () => {
+    const response = await fetch(`${base_url}/recipes/complexSearch?apiKey=${api_key}&cuisine=korean&number=5`);
+    const data = await response.json()
+    return data.results;
 };
